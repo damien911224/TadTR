151d75582396dbdfcb807b97eb24ce6dc2cfe3d7
damien911224 <damien911224@gmail.com>
2023-01-26 14:39:38 +0900

    Modified

--- models/deformable_transformer.py
+++ models/deformable_transformer.py
@@ -25,7 +25,7 @@ from torch import nn, Tensor
 from torch.nn.init import xavier_uniform_, constant_, uniform_, normal_
 
 from util.misc import inverse_sigmoid
-from .ops.modules import MSDeformAttn
+from .ops.deform_attn.modules import MSDeformAttn
 
 class DeformableTransformer(nn.Module):
     def __init__(self, d_model=256, nhead=8,
