42037f995454b18b5717dee584635232cba6861b
damien911224 <damien911224@gmail.com>
2023-01-26 15:40:02 +0900

    Modified

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ def main(args):
         raise NotImplementedError
 
     model, criterion, postprocessors = build_model(cfg)
-    model, criterion, postprocessors = build_model(cfg)
 
     model.to(device)
     model_without_ddp = model
