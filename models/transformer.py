e3db515e3759f1ceb45175b27799c24b531125cf
damien911224 <damien911224@gmail.com>
2023-01-31 23:50:44 +0900

    Modified

--- models/transformer.py
+++ models/transformer.py
@@ -247,7 +247,8 @@ class DeformableTransformerDecoderLayer(nn.Module):
         if not cfg.disable_query_self_att:
             # self attention
             q = k = self.with_pos_embed(tgt, query_pos)
-            tgt2, Q_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1)).transpose(0, 1)
+            tgt2, Q_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
+            tgt2 = tgt2.transpose(0, 1)
 
             # tgt2, _ = self.cross_attn(self.with_pos_embed(tgt, query_pos + tgt_pos[0]),
             #                           reference_points,
