45d8594394dbdba4d0722b4f19b693d777f9dd71
damien911224 <damien911224@gmail.com>
2023-02-01 16:18:16 +0900

    Modified

--- models/transformer.py
+++ models/transformer.py
@@ -250,7 +250,7 @@ class DeformableTransformerDecoderLayer(nn.Module):
         return tgt
 
     def forward(self, tgt, query_pos, reference_points, src, src_pos, src_spatial_shapes, level_start_index, src_padding_mask=None):
-        query_pos = None
+        # query_pos = None
         if not cfg.disable_query_self_att:
             # self attention
             q = k = self.with_pos_embed(tgt, query_pos)
@@ -258,16 +258,16 @@ class DeformableTransformerDecoderLayer(nn.Module):
             tgt2, Q_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
             tgt2 = tgt2.transpose(0, 1)
 
-            # q = k = tgt
-            # _, C_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
-            # q = k = query_pos
-            # _, P_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
-            #
-            # N, Q, _ = Q_weights.shape
-            # Q_C = torch.bmm(Q_weights.flatten(1).unsqueeze(-2), C_weights.flatten(1).unsqueeze(-1)).mean()
-            # Q_P = torch.bmm(Q_weights.flatten(1).unsqueeze(-2), P_weights.flatten(1).unsqueeze(-1)).mean()
-            #
-            # print(Q_C.detach().cpu().numpy(), Q_P.detach().cpu().numpy())
+            q = k = tgt
+            _, C_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
+            q = k = query_pos
+            _, P_weights = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))
+
+            N, Q, _ = Q_weights.shape
+            Q_C = torch.bmm(Q_weights.flatten(1).unsqueeze(-2), C_weights.flatten(1).unsqueeze(-1)).mean()
+            Q_P = torch.bmm(Q_weights.flatten(1).unsqueeze(-2), P_weights.flatten(1).unsqueeze(-1)).mean()
+
+            print(Q_C.detach().cpu().numpy(), Q_P.detach().cpu().numpy())
 
             tgt = tgt + self.dropout2(tgt2)
             tgt = self.norm2(tgt)
@@ -275,16 +275,16 @@ class DeformableTransformerDecoderLayer(nn.Module):
         else:
             pass
         # cross attention
-        tgt2, _ = self.cross_attn(self.with_pos_embed(tgt, query_pos),
-                               reference_points,
-                               src, src_spatial_shapes, level_start_index, src_padding_mask)
+        # tgt2, _ = self.cross_attn(self.with_pos_embed(tgt, query_pos),
+        #                        reference_points,
+        #                        src, src_spatial_shapes, level_start_index, src_padding_mask)
         # tgt2, _ = self.cross_attn(query_pos,
         #                        reference_points,
         #                        src, src_spatial_shapes, level_start_index, src_padding_mask)
-        # tgt2 = self.cross_attn(query=self.with_pos_embed(tgt, query_pos).transpose(0, 1),
-        #                        key=self.with_pos_embed(src, src_pos).transpose(0, 1),
-        #                        value=src.transpose(0, 1),
-        #                        key_padding_mask=src_padding_mask)[0].transpose(0, 1)
+        tgt2 = self.cross_attn(query=self.with_pos_embed(tgt, query_pos).transpose(0, 1),
+                               key=self.with_pos_embed(src, src_pos).transpose(0, 1),
+                               value=src.transpose(0, 1),
+                               key_padding_mask=src_padding_mask)[0].transpose(0, 1)
         tgt = tgt + self.dropout1(tgt2)
         tgt = self.norm1(tgt)
 
