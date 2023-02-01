49ebacdff0562d8a23f61598d50e2533c8adb3a3
damien911224 <damien911224@gmail.com>
2023-02-01 22:23:00 +0900

    Modified

--- models/dab_transformer.py
+++ models/dab_transformer.py
@@ -410,7 +410,8 @@ class TransformerDecoderLayer(nn.Module):
             #                 F.normalize(P_weights.flatten(1)).unsqueeze(-1)).mean()
             #
             # print(Q_C.detach().cpu().numpy(), Q_P.detach().cpu().numpy())
-
+            print(Q_weights.shape)
+            exit()
             print(torch.argsort(Q_weights[0].detach().cpu(), dim=-1)[..., :10].numpy())
 
             tgt = tgt + self.dropout1(tgt2)
