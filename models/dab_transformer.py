03a178d18cf1b44d66aae19a72f147ce4bdffa5c
damien911224 <damien911224@gmail.com>
2023-02-07 22:11:10 +0900

    Modified

--- models/dab_transformer.py
+++ models/dab_transformer.py
@@ -462,7 +462,8 @@ class TransformerDecoderLayer(nn.Module):
         q = torch.cat([q, query_sine_embed], dim=3).view(num_queries, bs, n_model * 2)
         k = k.view(hw, bs, self.nhead, n_model//self.nhead)
         k_pos = k_pos.view(hw, bs, self.nhead, n_model//self.nhead)
-        k = torch.cat([k, k_pos], dim=3).view(hw, bs, n_model * 2)
+        # k = torch.cat([k, k_pos], dim=3).view(hw, bs, n_model * 2)
+        k = torch.cat([k, k], dim=3).view(hw, bs, n_model * 2)
 
         tgt2 = self.cross_attn(query=q,
                                key=k,
