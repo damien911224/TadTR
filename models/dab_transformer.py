ed1d030d88fc0e634ffac2ae6d18f2182e48883f
damien911224 <damien911224@gmail.com>
2023-02-07 22:08:05 +0900

    Modified

--- models/dab_transformer.py
+++ models/dab_transformer.py
@@ -307,12 +307,12 @@ class TransformerEncoderLayer(nn.Module):
                      src_mask: Optional[Tensor] = None,
                      src_key_padding_mask: Optional[Tensor] = None,
                      pos: Optional[Tensor] = None):
-        # q = k = self.with_pos_embed(src, pos)
-        q = k = src
+        q = k = self.with_pos_embed(src, pos)
+        # q = k = src
         src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask,
                                           key_padding_mask=src_key_padding_mask)
 
-        print(torch.argsort(-K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
+        # print(torch.argsort(-K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
 
         src = src + self.dropout1(src2)
         src = self.norm1(src)
