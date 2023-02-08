b7765a62f3fcc4a221ad1b1ae82f64c92e65f312
damien911224 <damien911224@gmail.com>
2023-02-08 21:42:04 +0900

    Modified

--- models/dab_transformer.py
+++ models/dab_transformer.py
@@ -302,20 +302,19 @@ class TransformerEncoderLayer(nn.Module):
     def with_pos_embed(self, tensor, pos: Optional[Tensor]):
         return tensor if pos is None else tensor + pos
 
-    def forward(self,
-                     src,
-                     src_mask: Optional[Tensor] = None,
-                     src_key_padding_mask: Optional[Tensor] = None,
-                     pos: Optional[Tensor] = None):
+    def forward(self, src,
+                src_mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
         q = k = self.with_pos_embed(src, pos)
         # q = k = src
-        src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                                          key_padding_mask=src_key_padding_mask)
+        src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
 
         # print(torch.argsort(-K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
 
         src = src + self.dropout1(src2)
         src = self.norm1(src)
+
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
         src = src + self.dropout2(src2)
         src = self.norm2(src)
