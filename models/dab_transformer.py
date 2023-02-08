d67216ca8abd34c62ffc0884c4078fcc4dfa33cb
damien911224 <damien911224@gmail.com>
2023-02-08 22:24:25 +0900

    Modified

--- models/dab_transformer.py
+++ models/dab_transformer.py
@@ -313,7 +313,7 @@ class TransformerEncoderLayer(nn.Module):
         src2, K_weights = self.self_attn(q, k, value=src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
 
         # print(torch.argsort(-K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
-        print(torch.max(K_weights[0].detach().cpu(), dim=-1)[0][:10])
+        # print(torch.max(K_weights[0].detach().cpu(), dim=-1)[0][:10])
 
         src = src + self.dropout1(src2)
         src = self.norm1(src)
