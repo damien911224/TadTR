1b959de876e3eb1a207710bd89190a0adadeb04a
damien911224 <damien911224@gmail.com>
2023-02-09 21:55:13 +0900

    p

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -325,7 +325,10 @@ class SetCriterion(nn.Module):
 
         N, Q, K = C_weights.shape
 
-        target_Q_weights = torch.softmax(torch.bmm(torch.log(C_weights), torch.log(C_weights).transpose(1, 2)), dim=-1)
+        target_Q_weights = torch.softmax(torch.bmm(C_weights, C_weights.transpose(1, 2)), dim=-1)
+        # target_Q_weights = torch.softmax(torch.bmm(torch.log(C_weights),
+        #                                            torch.log(C_weights).transpose(1, 2)), dim=-1)
+
         # print(torch.max(target_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
         print(target_Q_weights[0, 0].detach().cpu().numpy())
 
--- models/attention.py
+++ models/attention.py
@@ -500,6 +500,8 @@ def multi_head_attention_forward(query: Tensor,
         )
         attn_output_weights = attn_output_weights.view(bsz * num_heads, tgt_len, src_len)
 
+    unnormalized_attn_output_weights = attn_output_weights
+
     # attn_output_weights = softmax(
     #     attn_output_weights, dim=-1)
     attn_output_weights = softmax(
@@ -513,7 +515,8 @@ def multi_head_attention_forward(query: Tensor,
 
     if need_weights:
         # average attention weights over heads
-        attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+        # attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+        attn_output_weights = unnormalized_attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
         return attn_output, attn_output_weights.sum(dim=1) / num_heads
     else:
         return attn_output, None
