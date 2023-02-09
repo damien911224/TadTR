5a0504b021fe5adc1b62ff99bd045110b51fad3d
damien911224 <damien911224@gmail.com>
2023-02-09 23:18:57 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -328,14 +328,14 @@ class SetCriterion(nn.Module):
         # C_weights = F.softmax(C_weights, dim=-1)
         QQ_weights = torch.bmm(C_weights, C_weights.transpose(1, 2))
         # target_Q_weights = F.log_softmax(QQ_weights, dim=-1)
-        target_Q_weights = F.softmax(QQ_weights * 50.0, dim=-1)
+        # target_Q_weights = F.softmax(QQ_weights * 50.0, dim=-1)
         target_Q_weights = F.softmax(QQ_weights * (torch.max(C_weights.log() / torch.max(QQ_weights))), dim=-1)
         # target_Q_weights = F.log_softmax(QQ_weights * 10000.0, dim=-1)
         # target_Q_weights = F.log_softmax(torch.bmm(torch.log(C_weights),
         #                                            torch.log(C_weights).transpose(1, 2)), dim=-1)
 
         # print(torch.argsort(-target_Q_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
-        # print(torch.max(target_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
+        print(torch.max(target_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
         # print(torch.max(C_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
         # print(target_Q_weights[0, 0].detach().cpu().numpy())
 
