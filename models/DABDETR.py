75961ceb6602de9d5938eac9d523c5899ac0f444
damien911224 <damien911224@gmail.com>
2023-02-09 21:25:01 +0900

    p

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -523,8 +523,8 @@ def build(args):
         weight_dict['loss_actionness'] = args.act_loss_coef
         losses.append('actionness')
 
-    weight_dict["loss_QQ"] = 1.0
-    losses.append("QQ")
+    # weight_dict["loss_QQ"] = 1.0
+    # losses.append("QQ")
 
     if args.aux_loss:
         aux_weight_dict = {}
