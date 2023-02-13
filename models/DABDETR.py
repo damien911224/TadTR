fcb55bf31cc841c069d374ba24e72a7bfa1608e4
damien911224 <damien911224@gmail.com>
2023-02-13 15:57:56 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -361,7 +361,7 @@ class SetCriterion(nn.Module):
         #                                            torch.log(C_weights).transpose(1, 2)), dim=-1)
 
         # print(torch.argsort(-target_Q_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
-        # print(torch.max(target_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
+        print(torch.max(target_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
         # print(torch.max(C_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
         # print(target_Q_weights[0, 0].detach().cpu().numpy())
         # print((torch.max(C_weights) - torch.max(target_Q_weights)).detach().cpu().numpy())
