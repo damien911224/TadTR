7b0977c9e5db7b34b2df67485351ca58eee1182b
damien911224 <damien911224@gmail.com>
2023-02-13 20:31:04 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -191,10 +191,10 @@ class TadTR(nn.Module):
             normalized_K_weights = torch.sqrt(torch.bmm(normalized_K_weights, K_weights[i + 1].transpose(1, 2)))
             normalized_K_weights = normalized_K_weights / torch.sum(normalized_K_weights, dim=-1, keepdim=True)
 
-        print(torch.argsort(-normalized_Q_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
-        print(torch.max(normalized_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
-        print(torch.argsort(-normalized_K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
-        print(torch.max(normalized_K_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
+        # print(torch.argsort(-normalized_Q_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
+        # print(torch.max(normalized_Q_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
+        # print(torch.argsort(-normalized_K_weights[0].detach().cpu(), dim=-1)[:10, :10].numpy())
+        # print(torch.max(normalized_K_weights[0].detach().cpu(), dim=-1)[0][:10].numpy())
 
         # out = {'pred_logits': outputs_class[-1], 'pred_segments': outputs_coord[-1],
         #        'Q_weights': Q_weights[-1], 'K_weights': K_weights[-1], 'C_weights': C_weights[-1]}
