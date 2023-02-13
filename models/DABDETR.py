b57c83b15e3e2e18d471974c63ae6d58dd899103
damien911224 <damien911224@gmail.com>
2023-02-13 18:00:50 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -182,11 +182,11 @@ class TadTR(nn.Module):
 
         outputs_class = self.class_embed(hs)
 
-        # out = {'pred_logits': outputs_class[-1], 'pred_segments': outputs_coord[-1],
-        #        'Q_weights': Q_weights[-1], 'K_weights': K_weights[-1], 'C_weights': C_weights[-1]}
         out = {'pred_logits': outputs_class[-1], 'pred_segments': outputs_coord[-1],
-               'Q_weights': torch.mean(Q_weights, dim=0), 'K_weights': torch.mean(K_weights, dim=0),
-               'C_weights': C_weights[-1]}
+               'Q_weights': Q_weights[-1], 'K_weights': K_weights[-1], 'C_weights': C_weights[-1]}
+        # out = {'pred_logits': outputs_class[-1], 'pred_segments': outputs_coord[-1],
+        #        'Q_weights': torch.mean(Q_weights, dim=0), 'K_weights': torch.mean(K_weights, dim=0),
+        #        'C_weights': C_weights[-1]}
         # out = {'pred_logits': outputs_class[-1], 'pred_segments': outputs_coord[-1],
         #        'Q_weights': torch.mean(Q_weights, dim=0), 'K_weights': torch.mean(K_weights, dim=0),
         #        'C_weights': torch.mean(C_weights, dim=0)}
@@ -608,11 +608,11 @@ def build(args):
         weight_dict['loss_actionness'] = args.act_loss_coef
         losses.append('actionness')
 
-    # weight_dict["loss_KK"] = 1.0
-    # losses.append("KK")
+    weight_dict["loss_KK"] = 1.0
+    losses.append("KK")
 
-    # weight_dict["loss_QQ"] = 1.0
-    # losses.append("QQ")
+    weight_dict["loss_QQ"] = 1.0
+    losses.append("QQ")
 
     if args.aux_loss:
         aux_weight_dict = {}
