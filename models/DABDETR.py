116c3936cdc2eb73961e3db5c2094000fc9bf67c
damien911224 <damien911224@gmail.com>
2023-02-13 15:56:14 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -478,8 +478,8 @@ class SetCriterion(nn.Module):
                     if 'actionness' in loss:
                         continue
 
-                    # if 'KK' in loss:
-                    #     continue
+                    if 'QQ' in loss or 'KK' in loss:
+                        continue
          
                     kwargs = {}
                     if loss == 'labels':
