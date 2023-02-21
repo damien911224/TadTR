6291ca74442dbcbe8d3012e4424a03e1475ede4f
damien911224 <damien911224@gmail.com>
2023-02-21 17:32:25 +0900

    Modified

--- engine.py
+++ engine.py
@@ -42,9 +42,6 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         outputs = model((samples.tensors, samples.mask))
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
-        print(loss_dict.keys())
-        print([k for k in loss_dict.keys() if k in weight_dict])
-        exit()
         losses = sum(loss_dict[k] * weight_dict[k]
                      for k in loss_dict.keys() if k in weight_dict)
         # reduce losses over all GPUs for logging purposes
