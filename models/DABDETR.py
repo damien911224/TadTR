451a1ff2aa6871230afb0a5b2a36faea2619f981
damien911224 <damien911224@gmail.com>
2023-02-21 17:22:29 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -229,7 +229,7 @@ class TadTR(nn.Module):
         return out
 
     @torch.jit.unused
-    def _set_aux_loss(self, outputs_class, outputs_coords):
+    def _set_aux_loss(self, outputs_class, outputs_coord):
         # this is a workaround to make torchscript happy, as torchscript
         # doesn't support dictionary with non-homogeneous values, such
         # as a dict having both a Tensor and a list.
