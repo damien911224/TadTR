89d6cde7f6d31229feee8ac48b7e988efbc97b41
damien911224 <damien911224@gmail.com>
2023-02-26 17:40:15 +0900

    Modified

--- models/transformer.py
+++ models/transformer.py
@@ -294,7 +294,8 @@ class DeformableTransformerDecoderLayer(nn.Module):
         tgt2, C_weights = self.cross_attn(query=self.with_pos_embed(tgt, query_pos).transpose(0, 1),
                                           key=self.with_pos_embed(src, src_pos).transpose(0, 1),
                                           value=src.transpose(0, 1),
-                                          key_padding_mask=src_padding_mask)[0].transpose(0, 1)
+                                          key_padding_mask=src_padding_mask)
+        tgt2 = tgt2.transpose(0, 1)
         tgt = tgt + self.dropout1(tgt2)
         tgt = self.norm1(tgt)
 
