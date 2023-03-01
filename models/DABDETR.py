042a69d2fd5e2c80d5f2c3dce4339cd33d63b94c
damien911224 <damien911224@gmail.com>
2023-03-02 00:43:05 +0900

    Modified

--- models/DABDETR.py
+++ models/DABDETR.py
@@ -629,7 +629,6 @@ class SetCriterion(nn.Module):
         # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
         if 'aux_outputs' in outputs:
             for i, aux_outputs in enumerate(outputs['aux_outputs']):
-                print(i)
                 indices = self.matcher(aux_outputs, targets)
                 for loss in self.losses:
                     # we do not compute actionness loss for aux outputs
