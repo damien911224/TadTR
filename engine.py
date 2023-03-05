4edde36921bc4122946789f711559706a97fc431
damien911224 <damien911224@gmail.com>
2023-03-05 17:31:18 +0900

    Modified

--- engine.py
+++ engine.py
@@ -182,6 +182,8 @@ def test(model, criterion, postprocessor, data_loader, base_ds, device, output_d
 
         if diversity:
             K_in = outputs["K_in"].detach().cpu().transpose(1, 0).numpy()
+            print(K_in.shape)
+            exit()
             K_out = outputs["K_out"].detach().cpu().transpose(1, 0).numpy()
             Q_in = outputs["Q_in"].detach().cpu().transpose(1, 0).numpy()
             Q_out = outputs["Q_out"].detach().transpose(1, 0).cpu().numpy()
