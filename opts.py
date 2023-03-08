68458974effc60dd8485177100b53e0d3e4ac485
damien911224 <damien911224@gmail.com>
2023-03-08 19:28:02 +0900

    Modified

--- opts.py
+++ opts.py
@@ -129,7 +129,7 @@ cfg.eos_coef = 0.1
 cfg.focal_alpha = 0.25
 
 # Set cost weight
-cfg.set_cost_class = 2  # Class coefficient # 2
+cfg.set_cost_class = 6  # Class coefficient
 cfg.set_cost_seg = 5  # Segment L1 coefficient
 cfg.set_cost_iou = 2  # Segment IoU coefficient
 
