bec6a5b8a00db83d1cc927233dce39f9e562ab44
damien911224 <damien911224@gmail.com>
2023-03-14 15:21:58 +0900

    Modified

--- opts.py
+++ opts.py
@@ -23,7 +23,7 @@ def get_args_parser():
 
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
-    parser.add_argument('--seed', default=42, type=int) # 42
+    parser.add_argument('--seed', default=16, type=int) # 42
 
     parser.add_argument('--resume', default='', help='resume from checkpoint')
 
