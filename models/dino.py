8ac3cf448d390128faa325f3bb0faaa094531620
damien911224 <damien911224@gmail.com>
2023-05-24 21:58:01 +0900

    Modified

--- models/dino.py
+++ models/dino.py
@@ -21,8 +21,8 @@ import torch.nn.functional as F
 from torch import nn
 from torchvision.ops.boxes import nms
 
-from util import segment_ops
 # from util import box_ops
+from util import segment_ops
 from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
                        accuracy, get_world_size,
                        is_dist_avail_and_initialized, inverse_sigmoid)
