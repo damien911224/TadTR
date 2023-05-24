65108897c6c6d447227f312d631a0a42c4c29139
damien911224 <damien911224@gmail.com>
2023-05-24 21:48:07 +0900

    Modified

--- models/dino.py
+++ models/dino.py
@@ -21,7 +21,7 @@ import torch.nn.functional as F
 from torch import nn
 from torchvision.ops.boxes import nms
 
-from util import box_ops
+# from util import box_ops
 from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
                        accuracy, get_world_size, interpolate,
                        is_dist_avail_and_initialized, inverse_sigmoid)
