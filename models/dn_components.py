befa95a87aa0a487b6ee7b048ed9464b9f380cfe
damien911224 <damien911224@gmail.com>
2023-05-24 21:50:45 +0900

    Modified

--- models/dn_components.py
+++ models/dn_components.py
@@ -13,7 +13,7 @@ from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
                        accuracy, get_world_size,
                        is_dist_avail_and_initialized, inverse_sigmoid)
 # from .DABDETR import sigmoid_focal_loss
-from util import box_ops
+# from util import box_ops
 import torch.nn.functional as F
 
 
