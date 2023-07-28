d7b634a7d17f16b16cca53f7c4d5cb3de45b6de4
damien <damien911224@gmail.com>
2023-07-29 00:45:47 +0900

    Modified

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -185,7 +185,7 @@ def get_dataset_info(dataset, feature):
             feature_info = {'local_path': path_info['thumos14'][feature]['local_path'], 'format': 'torch', 'fn_templ': '%s'}
             ft_info_file = path_info['thumos14'][feature]['ft_info_file']
         elif feature == "slowfast":
-            feature_info = {'local_path': path_info['thumos14'][feature]['local_path'], 'format': 'numpy', 'fn_templ': '%s.npy'}
+            feature_info = {'local_path': path_info['thumos14'][feature]['local_path'], 'format': 'np', 'fn_templ': '%s.npy'}
             ft_info_file = path_info['thumos14'][feature]['ft_info_file']
         else:
             raise ValueError('unsupported feature, should be one of [i3d2s]')
