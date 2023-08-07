f25a672a52b37614cdb29382ecf464f1be626372
damien <damien911224@gmail.com>
2023-08-07 20:19:38 +0900

    Modified

--- models/ops/temporal_deform_attn/temporal_deform_attn.py
+++ models/ops/temporal_deform_attn/temporal_deform_attn.py
@@ -13,12 +13,12 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from opts import cfg
+# from opts import cfg
 
 # if not cfg.disable_cuda:
 #     from .functions import TDAFunction
 
-from ..deform_attn.functions import MSDeformAttnFunction
+from ..functions import MSDeformAttnFunction
 
 import warnings
 import math
@@ -57,7 +57,7 @@ class DeformAttn(nn.Module):
             warnings.warn("You'd better set d_model in DeformAttn to make the dimension of each attention head a power of 2 "
                           "which is more efficient in our CUDA implementation.")
 
-        assert n_levels == 1, 'multi-level attention is not supported!'
+        # assert n_levels == 1, 'multi-level attention is not supported!'
 
         self.seq2col_step = 64
 
@@ -66,10 +66,8 @@ class DeformAttn(nn.Module):
         self.n_heads = n_heads
         self.n_points = n_points
 
-        self.sampling_offsets = nn.Linear(
-            d_model, n_heads * n_levels * n_points)
-        self.attention_weights = nn.Linear(
-            d_model, n_heads * n_levels * n_points)
+        self.sampling_offsets = nn.Linear(d_model, n_heads * n_levels * n_points)
+        self.attention_weights = nn.Linear(d_model, n_heads * n_levels * n_points)
         self.value_proj = nn.Linear(d_model, d_model)
         self.output_proj = nn.Linear(d_model, d_model)
 
@@ -79,12 +77,10 @@ class DeformAttn(nn.Module):
         constant_(self.sampling_offsets.weight.data, 0.)
         # Initial offsets:
         # (1, 0, -1, 0, -1, 0, 1, 0)
-        thetas = torch.arange(
-            self.n_heads, dtype=torch.float32) * (4.0 * math.pi / self.n_heads)
+        thetas = torch.arange(self.n_heads, dtype=torch.float32) * (4.0 * math.pi / self.n_heads)
         grid_init = thetas.cos()[:, None]
 
-        grid_init = grid_init.view(self.n_heads, 1, 1, 1).repeat(
-            1, self.n_levels, self.n_points, 1)
+        grid_init = grid_init.view(self.n_heads, 1, 1, 1).repeat(1, self.n_levels, self.n_points, 1)
         for i in range(self.n_points):
             grid_init[:, :, i, :] *= i + 1
 
@@ -116,15 +112,11 @@ class DeformAttn(nn.Module):
         value = self.value_proj(input_flatten)
         if input_padding_mask is not None:
             value = value.masked_fill(input_padding_mask[..., None], float(0))
-        value = value.view(N, Len_in, self.n_heads,
-                           self.d_model // self.n_heads)
+        value = value.view(N, Len_in, self.n_heads, self.d_model // self.n_heads)
         # the predicted offset in temporal axis. They are *absolute* values, not normalized
-        sampling_offsets = self.sampling_offsets(query).view(
-            N, Len_q, self.n_heads, self.n_levels, self.n_points, 1)
-        attention_weights = self.attention_weights(query).view(
-            N, Len_q, self.n_heads, self.n_levels * self.n_points)
-        attention_weights = F.softmax(
-            attention_weights, -1).view(N, Len_q, self.n_heads, self.n_levels, self.n_points)
+        sampling_offsets = self.sampling_offsets(query).view(N, Len_q, self.n_heads, self.n_levels, self.n_points, 1)
+        attention_weights = self.attention_weights(query).view(N, Len_q, self.n_heads, self.n_levels * self.n_points)
+        attention_weights = F.softmax(attention_weights, -1).view(N, Len_q, self.n_heads, self.n_levels, self.n_points)
 
         if reference_points.shape[-1] == 1:
             # the reference points are normalized, but the offset are unnormalized
@@ -136,26 +128,28 @@ class DeformAttn(nn.Module):
                 offset_normalizer[None, None, None, :, None, :]
         # deform attention in the l-th (l >= 2) decoder layer when segment refinement is enabled
         elif reference_points.shape[-1] == 2:
+            # cw_refpoints = torch.stack(((reference_points[..., 0] + reference_points[..., 1]) / 2,
+            #                              reference_points[..., 1] - reference_points[..., 0]), dim=-1)
             # offsets are related with the size of the reference segment
             sampling_locations = reference_points[:, :, None, :, None, :1] \
                 + sampling_offsets / self.n_points * \
                 reference_points[:, :, None, :, None, 1:] * 0.5
+        elif reference_points.shape[-1] == 3:
+            # cw_refpoints = torch.stack(((reference_points[..., 0] + reference_points[..., 1]) / 2,
+            #                              reference_points[..., 1] - reference_points[..., 0]), dim=-1)
+            # offsets are related with the size of the reference segment
+            sampling_locations = reference_points[:, :, None, :, None, :1] \
+                + sampling_offsets / self.n_points * \
+                reference_points[:, :, None, :, None, 1:-1] * 0.5
         else:
             raise ValueError(
                 'Last dim of reference_points must be 1 or 2, but get {} instead.'.format(reference_points.shape[-1]))
-        if cfg.dfm_att_backend == 'pytorch' or cfg.disable_cuda:
-            # Implementation with PyTorch grid_sample operator. 
-            # Note that grid_sample only supports image inputs. We need to view the sequence as an image with height=1
-            sampling_locations = torch.cat((sampling_locations, torch.ones_like(sampling_locations)*0.5), dim=-1)
-            input_spatial_shapes = torch.stack((torch.ones_like(input_temporal_lens), input_temporal_lens), dim=-1)
-            # output = deform_attn_core_pytorch(value, input_spatial_shapes, sampling_locations, attention_weights)
-            output = MSDeformAttnFunction.apply(value, input_spatial_shapes, input_level_start_index,
-                                                sampling_locations, attention_weights, self.seq2col_step)
-        else:
-            raise NotImplementedError
-            # # CUDA implementation. You will get identical results with the pytorch implementation
-            # output = TDAFunction.apply(
-            #     value, input_temporal_lens, input_level_start_index, sampling_locations, attention_weights, self.seq2col_step)
+
+        sampling_locations = torch.cat((sampling_locations, torch.ones_like(sampling_locations)*0.5), dim=-1)
+        input_spatial_shapes = torch.stack((torch.ones_like(input_temporal_lens), input_temporal_lens), dim=-1)
+        # output = deform_attn_core_pytorch(value, input_spatial_shapes, sampling_locations, attention_weights)
+        output = MSDeformAttnFunction.apply(value, input_spatial_shapes, input_level_start_index,
+                                            sampling_locations, attention_weights, self.seq2col_step)
         output = self.output_proj(output)
         return output, (sampling_locations, attention_weights)
 
