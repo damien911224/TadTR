faef1772125ce045f70536ac5294ba84b2238331
damien <damien911224@gmail.com>
2023-08-07 20:25:28 +0900

    Modified

--- models/ops/deform_attn/modules/ms_deform_attn.py
+++ models/ops/deform_attn/modules/ms_deform_attn.py
@@ -106,14 +106,13 @@ class MSDeformAttn(nn.Module):
         if reference_points.shape[-1] == 2:
             # offset_normalizer = torch.stack([input_spatial_shapes[..., 1], input_spatial_shapes[..., 0]], -1)
             # offset_normalizer = torch.stack([input_spatial_shapes[..., 0], input_spatial_shapes[..., 0]], -1)
-            t1t2_refpoints = \
-                torch.stack((torch.clamp(reference_points[..., 0] - reference_points[..., 1] / 2, 0.0, 1.0),
-                             torch.clamp(reference_points[..., 0] + reference_points[..., 1] / 2, 0.0, 1.0)),
-                            dim=-1)
-            # sampling_locations = t1t2_refpoints[:, :, None, :, None, :] \
+            # sampling_locations = reference_points[:, :, None, :, None, :] \
             #                      + sampling_offsets / offset_normalizer[None, None, None, :, None, :]
-            sampling_locations = t1t2_refpoints[:, :, None, :, None, :] \
-                                 + sampling_offsets / self.n_points * reference_points[:, :, None, :, None, 1][..., None] * 0.5
+            sampling_locations = torch.stack((reference_points[:, :, None, :, None, 0],
+                                              torch.ones_like(reference_points[:, :, None, :, None, 0]) * 0.5), dim=-1) \
+                                 + sampling_offsets / self.n_points * \
+                                 torch.stack((reference_points[:, :, None, :, None, 1],
+                                              torch.ones_like(reference_points[:, :, None, :, None, 1])), dim=-1) * 0.5
         # elif reference_points.shape[-1] == 3:
         #     # offset_normalizer = torch.stack([input_spatial_shapes[..., 1], input_spatial_shapes[..., 0]], -1)
         #     offset_normalizer = torch.stack([input_spatial_shapes[..., 0], input_spatial_shapes[..., 0]], -1)
@@ -146,30 +145,7 @@ class MSDeformAttn(nn.Module):
         else:
             raise ValueError(
                 'Last dim of reference_points must be 2 or 4, but get {} instead.'.format(reference_points.shape[-1]))
-        # output = MSDeformAttnFunction.apply(
-        #     value, input_spatial_shapes, input_level_start_index, sampling_locations, attention_weights, self.im2col_step)
-        output = ms_deform_attn_core_pytorch(value, input_spatial_shapes, sampling_locations, attention_weights)
+        output = MSDeformAttnFunction.apply(
+            value, input_spatial_shapes, input_level_start_index, sampling_locations, attention_weights, self.im2col_step)
         output = self.output_proj(output)
         return output
-
-def ms_deform_attn_core_pytorch(value, value_spatial_shapes, sampling_locations, attention_weights):
-    # for debug and test only,
-    # need to use cuda version instead
-    N_, S_, M_, D_ = value.shape
-    _, Lq_, M_, L_, P_, _ = sampling_locations.shape
-    value_list = value.split([H_ * W_ for H_, W_ in value_spatial_shapes], dim=1)
-    sampling_grids = 2 * sampling_locations - 1
-    sampling_value_list = []
-    for lid_, (H_, W_) in enumerate(value_spatial_shapes):
-        # N_, H_*W_, M_, D_ -> N_, H_*W_, M_*D_ -> N_, M_*D_, H_*W_ -> N_*M_, D_, H_, W_
-        value_l_ = value_list[lid_].flatten(2).transpose(1, 2).reshape(N_*M_, D_, H_, W_)
-        # N_, Lq_, M_, P_, 2 -> N_, M_, Lq_, P_, 2 -> N_*M_, Lq_, P_, 2
-        sampling_grid_l_ = sampling_grids[:, :, :, lid_].transpose(1, 2).flatten(0, 1)
-        # N_*M_, D_, Lq_, P_
-        sampling_value_l_ = F.grid_sample(value_l_, sampling_grid_l_,
-                                          mode='bilinear', padding_mode='zeros', align_corners=False)
-        sampling_value_list.append(sampling_value_l_)
-    # (N_, Lq_, M_, L_, P_) -> (N_, M_, Lq_, L_, P_) -> (N_, M_, 1, Lq_, L_*P_)
-    attention_weights = attention_weights.transpose(1, 2).reshape(N_*M_, 1, Lq_, L_*P_)
-    output = (torch.stack(sampling_value_list, dim=-2).flatten(-2) * attention_weights).sum(-1).view(N_, M_*D_, Lq_)
-    return output.transpose(1, 2).contiguous()
