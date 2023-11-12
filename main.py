ed1437453c8446a9fdceb060f14d7ed3b81447d6
damien <damien911224@gmail.com>
2023-11-12 21:31:03 +0900

    Modified

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ def main(args):
         logging.info('best det result\n{}'.format(best_metric_txt))
         logging.info(log_path)
 
-        if best_metric >= 0.568:
+        if best_metric > 0.572:
             break
 
 
